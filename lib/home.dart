174144a4f609f9a3e6f06f814370dc676392aa7d
DanielDeveloper08 <danielprogramando2000@gmail.com>
2022-09-24 10:41:58 -0500

    Firebase login ok

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project_cinema/consultMovie.dart';
 import 'package:flutter_project_cinema/consultProduct.dart';
+import 'package:flutter_project_cinema/main.dart';
 import 'package:flutter_project_cinema/newMovie.dart';
 import 'package:flutter_project_cinema/newProduct.dart';
 
@@ -22,97 +24,160 @@ class Home extends StatelessWidget {
               image: NetworkImage(
                   "https://media.admagazine.com/photos/618a65f958ac69e38abb60ba/3:4/w_1125,h_1500,c_limit/73368.jpg"),
               fit: BoxFit.cover)),
-      child: ListView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          movieTittle(),
+          titulo("<<< Peliculas >>>"),
+          const SizedBox(width: 5),
+          imagenPelicula(context),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               registrarPelicula(context),
-              const SizedBox(width: 5),
+              const SizedBox(width: 10),
               consultarPelicula(context),
             ],
           ),
-          productTittle(),
+          const SizedBox(height: 60),
+          titulo("<<< Productos >>>"),
+          imagenProducto(context),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               registrarProducto(context),
-              const SizedBox(width: 5),
+              const SizedBox(width: 10),
               consultarProducto(context),
             ],
           ),
+          const SizedBox(height: 60),
+          cerrarSesion(context)
         ],
       ),
     );
   }
 
-  Widget movieTittle() {
-    return const Text(
-      "Peliculas",
-      style: TextStyle(color: Colors.white, fontSize: 20),
+  Widget imagenProducto(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: double.infinity,
+      height: (screenWidth - 80.0) * 2.0 / 4.0,
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      child: Image.network(
+          'https://cdn-icons-png.flaticon.com/512/4221/4221407.png'),
+    );
+  }
+
+  Widget imagenPelicula(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: double.infinity,
+      height: (screenWidth - 80.0) * 2.0 / 4.0,
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      child: Image.network(
+          'https://cdn-icons-png.flaticon.com/512/2503/2503529.png'),
     );
   }
 
   Widget registrarPelicula(BuildContext context) {
     return SizedBox(
-      height: 30,
+      height: 40,
+      width: 150,
       child: ElevatedButton(
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: ((context) => const NewMovie())));
         },
         style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
-        child: const Text("Registrar"),
+        child: const Text(
+          "Registrar",
+          style: TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
 
   Widget consultarPelicula(BuildContext context) {
     return SizedBox(
-      height: 30,
+      height: 40,
+      width: 150,
       child: ElevatedButton(
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: ((context) => ConsultMovie())));
         },
         style: TextButton.styleFrom(backgroundColor: Colors.amber),
-        child: const Text("Consultar"),
+        child: const Text(
+          "Consultar",
+          style: TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
 
-  Widget productTittle() {
-    return const Text(
-      "Productos",
-      style: TextStyle(color: Colors.white, fontSize: 20),
+  Widget titulo(String titulo) {
+    return Text(
+      titulo,
+      style: const TextStyle(
+          color: Colors.black,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          backgroundColor: Colors.white),
     );
   }
 
   Widget registrarProducto(BuildContext context) {
     return SizedBox(
-      height: 30,
+      height: 40,
+      width: 150,
       child: ElevatedButton(
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: ((context) => const NewProduct())));
         },
         style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
-        child: const Text("Registrar"),
+        child: const Text(
+          "Registrar",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ),
     );
   }
 
   Widget consultarProducto(BuildContext context) {
     return SizedBox(
-      height: 30,
+      height: 40,
+      width: 150,
       child: ElevatedButton(
         onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: ((context) => ConsultProduct())));
+          Navigator.push(context,
+              MaterialPageRoute(builder: ((context) => ConsultProduct())));
         },
         style: TextButton.styleFrom(backgroundColor: Colors.amber),
-        child: const Text("Consultar"),
+        child: const Text(
+          "Consultar",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  Widget cerrarSesion(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      width: 250,
+      child: ElevatedButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut().then((value) => Navigator.push(
+              context,
+              MaterialPageRoute(builder: ((context) => const MyApp()))));
+        },
+        style: TextButton.styleFrom(backgroundColor: Colors.redAccent),
+        child: const Text(
+          "Cerrar Sesion",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
--- lib/reset_password.dart
+++ lib/reset_password.dart
@@ -44,11 +44,14 @@ class _ResetPasswordState extends State<ResetPassword> {
                 const SizedBox(
                   height: 20,
                 ),
-                /*firebaseUIButton(context, "Reset Password", () {
-                  FirebaseAuth.instance
-                      .sendPasswordResetEmail(email: _emailTextController.text)
-                      .then((value) => Navigator.of(context).pop());
-                })*/
+                firebaseUIButton(context, "Reset Password", () {
+                  if (_emailTextController.text.isNotEmpty) {
+                    FirebaseAuth.instance
+                        .sendPasswordResetEmail(
+                            email: _emailTextController.text)
+                        .then((value) => Navigator.of(context).pop());
+                  }
+                })
               ],
             ),
           ))),
@@ -81,6 +84,36 @@ class _ResetPasswordState extends State<ResetPassword> {
       keyboardType: isPasswordType
           ? TextInputType.visiblePassword
           : TextInputType.emailAddress,
+    );
+  }
+
+  Container firebaseUIButton(
+      BuildContext context, String title, Function onTap) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+      child: ElevatedButton(
+        onPressed: () {
+          onTap();
+        },
+        child: Text(
+          title,
+          style: const TextStyle(
+              color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.black26;
+              }
+              return Colors.redAccent;
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)))),
+      ),
     );
   }
 }
